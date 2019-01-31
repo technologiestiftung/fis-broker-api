748842633c760e1fe72d96c73255f9947819933e
Sebastian Meier <contact@sebastianmeier.eu>
2019-01-31 07:59:39 +0100

    new features and fixes
    Rate Limiting added
    CORS Header added
    event handling fixed for http-request / API

--- handler.js
+++ handler.js
@@ -3,29 +3,58 @@
 const { getWFSInfo, tryOutOptionsForLayerName } = require('./info.js');
 const { downloadGeoJson } = require('./download.js');
 
+const { RateLimiterMemory } = require('rate-limiter-flexible');
+
+const rateLimiter = new RateLimiterMemory({
+  points: 10,
+  duration: 300,
+});
+    
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true
+};
+
 var createResponse = function(info) {
     let response;
 
     if(info && info.length > 0) {
       response = {
+        headers:headers,
         statusCode: 200,
-        body: JSON.stringify(info),
+        body: JSON.stringify(info)
       };
     } else {
       response = {
+        headers:headers,
         statusCode: 400,
-        body: 'Somethign went wrong'
+        body: 'Something went wrong'
       };
     }
     return response;
 };
 
 var getInfoHandler = async (event) => {
-  const layerName = event['layer_name'];
+
+  const layerName = event['layer_name'] || event.headers['layer_name'];
   let info;
+
   try {
-    if (event['url']) {
-      info = await getWFSInfo(event['url']);
+    await rateLimiter.consume(('requestContext' in event) ? event.requestContext.identity.sourceIp : "USER_TOKEN", 1);
+  } catch (rejRes) {
+    return {
+      statusCode: 429,
+      body: JSON.stringify({message: 'Too Many Requests'})
+    }
+  }
+
+  let url = false;
+  if('url' in event) url = event.url;
+  if(('headers' in event) && ('url' in event.headers)) url = event.headers.url
+
+  try {
+    if (url) {
+      info = await getWFSInfo(url);
     } else {
       info = await tryOutOptionsForLayerName(layerName);
     }
@@ -33,13 +62,24 @@ var getInfoHandler = async (event) => {
       console.log(err);
       return err;
   }
+
   return(createResponse(info));
 };
 
 var getDownloadHandler = async (event) => {
-  const layerName = event['layer_name'];
-  const layerTypeName = event['layer_type'];
+  const layerName = event['layer_name'] || event.headers['layer_name'];
+  const layerTypeName = event['layer_type'] || event.headers['layer_type'];
   let result;
+
+  try {
+    await rateLimiter.consume(('requestContext' in event) ? event.requestContext.identity.sourceIp : "USER_TOKEN", 5);
+  } catch (rejRes) {
+    return {
+      statusCode: 429,
+      body: JSON.stringify({message: 'Too Many Requests'})
+    }
+  }
+
   try {
     result = await downloadGeoJson(layerName, layerTypeName);
   }
@@ -47,9 +87,10 @@ var getDownloadHandler = async (event) => {
       console.log(err);
       return err;
   }
+
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/octet-stream', 'Content-Disposition': 'attachment;filename='+layerName+'.geojson' },
+    headers: {...headers, 'Content-Type': 'application/octet-stream', 'Content-Disposition': 'attachment;filename='+layerName+'.geojson' },
     body: JSON.stringify(result)
   }
 };
